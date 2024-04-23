a2ffe99cf4ca86fcaafdb11f69aa218a3989d7a1
neptunusss <167156168+neptunusss@users.noreply.github.com>
2024-04-23 11:40:42 +0800

    Add files via upload

---
+++ stenley/index.php
@@ -0,0 +1,6 @@
+<h1>List Website Praktikum</h1>
+
+<li>
+    <a href="latprak1.php">hai</a>
+    <a href="latprak2.php">boy</a>
+</li>
---
+++ stenley/perulangan6.php
@@ -0,0 +1,8 @@
+<?php
+$d = 1;
+do{
+    
+echo "Perulangan NIM Mahasiswa 10101001 ke-$d <br>";
+$d++;
+}while($d <= 20);
+?>
